d32cb6bf59b6b07041c95d8a26c3da970d496e60
abdi-ali1 <62744253+abdi-ali1@users.noreply.github.com>
2022-07-21 20:16:02 +0200

    Insert all maps

---
+++ FibonacciOefenen/Fibonacci/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci();
+
+
+        }
+
+        /// <summary>
+        /// excutes the Fibonacci
+        /// </summary>
+        static void Fibonacci()
+        {
+            int number1 = 0;
+            int number2 = 1;
+            int number3 = 0;
+
+
+            int count = 20;
+            for (int i = 0; i < count; i++)
+            {
+                number3 = number1 + number2;
+                number1 = number2;
+                number2 = number3;
+
+                Console.WriteLine(number3);
+
+            }
+        }
+    }
+}
